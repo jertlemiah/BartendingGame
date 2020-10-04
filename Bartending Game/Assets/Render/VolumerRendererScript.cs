4ab8766ee47bdc2dc6504a79d44e465984ec9d28
jertlemiah <jeremiah.plauche@gmail.com>
2020-10-04 00:35:25 -0500

    Liquid level diplay renderer tweaks
    Briefy messing around with the liquid level display renderer. I'm not sure I can get it to work, but I should be able to instantiate new game objects, each with their own liquid level

---
+++ Bartending Game/Assets/Render/VolumerRendererScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumerRendererScript : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public float Offset = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        spriteRenderer.material.SetFloat("_Offset", Offset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRenderer.material.SetFloat("_LiquidLevel",.2f);
+        //_Color
+        //_Thickness
+        //_Offset
+    }
+}
