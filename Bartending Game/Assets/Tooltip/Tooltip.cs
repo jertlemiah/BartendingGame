68e24c743beb50354d11c793e3808febbebf4ee7
jertlemiah <jeremiah.plauche@gmail.com>
2020-10-11 23:29:35 -0500

    Tooltip work

--- Bartending Game/Assets/Tooltip/Tooltip.cs
+++ Bartending Game/Assets/Tooltip/Tooltip.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using CodeMonkey.Utils;
+using TMPro;
 
 public class Tooltip : MonoBehaviour {
 
@@ -10,15 +11,18 @@ public class Tooltip : MonoBehaviour {
 
     [SerializeField]
     private Camera uiCamera;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float padding;
 
-    private Text tooltipText;
+    private TextMeshProUGUI tooltipText;
     private RectTransform backgroundRectTransform;
 
     private void Awake() {
         instance = this;
         backgroundRectTransform = transform.Find("background").GetComponent<RectTransform>();
-        tooltipText = transform.Find("text").GetComponent<Text>();
+        tooltipText = transform.Find("text").GetComponent<TextMeshProUGUI>();
 
+        //ShowTooltip("Random tooltip text");
         HideTooltip();
         /*
         ShowTooltip("Random tooltip text");
@@ -35,9 +39,40 @@ public class Tooltip : MonoBehaviour {
     }
 
     private void Update() {
+        // Get mouse position
         Vector2 localPoint;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponent<RectTransform>(), Input.mousePosition, uiCamera, out localPoint);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            transform.parent.GetComponent<RectTransform>(), 
+            Input.mousePosition, uiCamera, out localPoint);
+
+        // Set tooltip location to mouse position
         transform.localPosition = localPoint;
+
+
+    }
+
+    private void FollowCursor()
+    {
+        //if (!transform.activeSelf) { return; }
+
+        //Vector3 newPos = Input.mousePosition + offset;
+        //newPos.z = 0f;
+        //float rightEdgeToScreenEdgeDistance = Screen.width - (newPos.x + popupObject.rect.width * popupCanvas.scaleFactor / 2) - padding;
+        //if (rightEdgeToScreenEdgeDistance < 0)
+        //{
+        //    newPos.x += rightEdgeToScreenEdgeDistance;
+        //}
+        //float leftEdgeToScreenEdgeDistance = 0 - (newPos.x - rect.width * popupCanvas.scaleFactor / 2) + padding;
+        //if (leftEdgeToScreenEdgeDistance > 0)
+        //{
+        //    newPos.x += leftEdgeToScreenEdgeDistance;
+        //}
+        //float topEdgeToScreenEdgeDistance = Screen.height - (newPos.y + popupObject.rect.height * popupCanvas.scaleFactor) - padding;
+        //if (topEdgeToScreenEdgeDistance < 0)
+        //{
+        //    newPos.y += topEdgeToScreenEdgeDistance;
+        //}
+        //transform.position = newPos;
     }
 
     private void ShowTooltip(string tooltipString) {
